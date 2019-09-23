69bd1d001c658ab6bf2da8d74e0676ff85a56762
Jason <azoulayjason@gmail.com>
2019-09-23 02:26:51 +0200

    lint

--- tests/auth.js
+++ tests/auth.js
@@ -5,12 +5,11 @@ var request = require('supertest');
 
 describe('Auth', function(done){
 
-    //let's set up the data we need to pass to the login method
     const userCredentials = {
         email: 'test@gmail.com', 
         password: 'test'
     }
-    //now let's login the user before we run any tests
+    //login the user before we run any tests
     var authenticatedUser = request.agent(app);
     before(done => {
         authenticatedUser
@@ -22,7 +21,6 @@ describe('Auth', function(done){
             done();
         });
     });
-//addresses 1st bullet point: if the user is logged in we should get a 200 status code
     it('should return a 200 response if the user is logged in', function(done){
     authenticatedUser.get('/api/special')
     .set('Authorization', 'Bearer ' + jwtToken)
@@ -30,6 +28,3 @@ describe('Auth', function(done){
     });
 
 });
-//this test says: make a POST to the /login route with the email: sponge@bob.com, password: garyTheSnail
-//after the POST has completed, make sure the status code is 200 
-//also make sure that the user has been directed to the /home page
