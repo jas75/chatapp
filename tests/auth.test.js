4823ae59c7e9e5aee02eb6f479ad6bb5c07c1740
Jason <azoulayjason@gmail.com>
2019-09-24 03:24:10 +0200

    Correct test

--- tests/auth.test.js
+++ tests/auth.test.js
@@ -6,29 +6,28 @@ const request = require('supertest');
 const logger = require('../utils/logger');
 
 describe('Auth Service', (done) => {
-  const userCredentials = {
-    email: 'test@gmail.com',
-    password: 'test'
-  };
-  // login the user before we run any tests
-  const authenticatedUser = request.agent(app);
-
-  before(done => {
-    authenticatedUser
-      .post('/api/login')
-      .send(userCredentials)
-      .end((err, response) => {
-        if (err) {
-          logger.error(err);
-        } else {
-          jwtToken = response.body.token;
-          expect(response.statusCode).to.equal(200);
-          done();
-        }
-      });
-  });
-
   describe('Passport.js', done => {
+    const userCredentials = {
+      email: 'test@gmail.com',
+      password: 'test'
+    };
+    // login the user before we run any tests
+    const authenticatedUser = request.agent(app);
+
+    before(done => {
+      authenticatedUser
+        .post('/api/login')
+        .send(userCredentials)
+        .end((err, response) => {
+          if (err) {
+            logger.error(err);
+          } else {
+            jwtToken = response.body.token;
+            expect(response.statusCode).to.equal(200);
+            done();
+          }
+        });
+    });
     it('should return status 200 if the user is logged in', (done) => {
       authenticatedUser.get('/api/special')
         .set('Authorization', 'Bearer ' + jwtToken)
